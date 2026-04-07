0d0de43c2e17802e4a6ee025d2c6b6da00a0d81d
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-04-07 20:13:31 -0300

    :alien: Update(CreateBetDto): From class to record

--- UniBet/Contexts/Betting/Application/UseCases/CreateBet/CreateBetDTO.cs
+++ UniBet/Contexts/Betting/Application/UseCases/CreateBet/CreateBetDTO.cs
@@ -1,17 +1,17 @@
 namespace UniBet.Contexts.Betting.Application.UseCases;
 
-public class CreateBetDTO
+public record CreateBetDTO
 {
     public Guid UserId { get; set; }
     public Guid GameId { get; set; }
     public decimal Amount { get; set; }
     public string Team { get; set; }
 
-    public CreateBetDTO(Guid userId, Guid gameId, decimal amount, string team)
-    {
-        UserId = userId;
-        GameId = gameId;
-        Amount = amount;
-        Team = team;
-    }
+    // public CreateBetDTO(Guid userId, Guid gameId, decimal amount, string team)
+    // {
+    //     UserId = userId;
+    //     GameId = gameId;
+    //     Amount = amount;
+    //     Team = team;
+    // }
 }
